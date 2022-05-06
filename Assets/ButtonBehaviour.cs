4c49fcaed50ed66796a56a66814de53f1a29cb0f
benjavitorgan <vitorganbenjamin@gmail.com>
2022-05-06 14:29:22 -0300

    Button, While and Instantiate

---
+++ Assets/ButtonBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public void ShowMessageInConsole ()
+    {
+        Debug.Log("Hello World");
+    }
+}
